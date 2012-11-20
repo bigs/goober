7399bdf379901b44a7f191f2982914e4f363086b
Cole Brown <bigswim@gmail.com>
2012-11-20 03:35:26 -0500

    Initial commit

---
+++ src/goober/goober.go
@@ -0,0 +1,105 @@
+package goober
+
+import  (
+  "net/http"
+  "container/list"
+  "strings"
+  "fmt"
+)
+
+type Goober struct {
+  head map[string]*routeTreeNode
+}
+
+type routeTreeNode struct {
+  handlers *list.List
+  children map[string]*routeTreeNode
+  dynamicChildren map[string]*routeTreeNode
+}
+
+func newRouteTreeNode() (node *routeTreeNode) {
+  node = new(routeTreeNode)
+  node.handlers = list.New()
+  node.children = make(map[string]*routeTreeNode)
+  node.dynamicChildren = make(map[string]*routeTreeNode)
+
+  return
+}
+
+func New() (* Goober) {
+  var g = new(Goober)
+  var head = make(map[string]*routeTreeNode)
+  g.head = head
+  g.head["GET"] = newRouteTreeNode()
+  g.head["POST"] = newRouteTreeNode()
+  g.head["PUT"] = newRouteTreeNode()
+  g.head["DELETE"] = newRouteTreeNode()
+
+  return g
+}
+
+func isSlash(s rune) (bool) {
+  return s == '/'
+}
+
+func (g *Goober) AddHandler(method string, route string, handler http.Handler) (err int){
+  err = 0
+  route = strings.TrimFunc(route, isSlash)
+  var parts = strings.Split(route, "/")
+
+  var cur = g.head[method]
+  for i := range parts {
+    var part = parts[i]
+
+    if (len(part) == 0) {
+      err = 1
+      return
+    }
+
+    // TODO: check if the current piece is a :variable or not
+    if strings.HasPrefix(part, ":") {
+      // dynamic
+      if (cur.dynamicChildren[part] != nil) {
+        cur = cur.dynamicChildren[part]
+      } else {
+        var next = newRouteTreeNode()
+        cur.dynamicChildren[part] = next
+        cur = next
+      }
+    } else {
+      if (cur.children[part] != nil) {
+        cur = cur.children[part]
+      } else {
+        var next = newRouteTreeNode()
+        cur.children[part] = next
+        cur = next
+      }
+    }
+  }
+  // add handler
+  cur.handlers.PushBack(handler)
+  return
+}
+
+func (g *Goober) Get(route string, handler http.Handler) {
+  g.AddHandler("GET", route, handler)
+}
+
+func (g *Goober) Post(route string, handler http.Handler) {
+  g.AddHandler("POST", route, handler)
+}
+
+func walkTree(node map[string]*routeTreeNode, parts *[]string, i int) (handler http.Handler, err int) {
+  handler = nil
+  err = 1
+  return
+}
+
+func (g *Goober) GetHandlers(method string, path string) {
+  path = strings.TrimFunc(path, isSlash)
+  var parts = strings.Split(path, "/")
+  for i := range parts {
+    fmt.Println(parts[i])
+  }
+}
+
